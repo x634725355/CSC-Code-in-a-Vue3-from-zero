86ba78bb5d5a5c8937bb0833ad051e9508f99fcf
澄三彩 <634725355@qq.com>
2022-05-06 15:57:44 +0800

    棋盘初始化

--- src/pages/Game/ChineseChess/ChineseChess.vue
+++ src/pages/Game/ChineseChess/ChineseChess.vue
@@ -1,36 +1,102 @@
 <script lang="ts" setup>
 import { ref, onMounted, reactive } from "vue";
 
+export interface ChessPieces {
+    name: string;
+    top: number;
+    left: number;
+    camp: "red" | "dark";
+}
+
 const globalParameter = reactive({
     horizontal: 11,
     vertical: 11,
 });
 
-const chessPieces = reactive({
-    red: {},
-    dark: {},
-})
+const size = ref(30);
 
+const radius = ref((size.value - 4) / 2);
+
+const width = ref((globalParameter.horizontal - 1) * size.value);
+const height = ref((globalParameter.vertical - 1) * size.value);
+
+const chessType = [
+    "car",
+    "horse",
+    "elephant",
+    "bodyguard",
+    "general",
+    "bodyguard",
+    "elephant",
+    "horse",
+    "car",
+];
+
+const chessText = {
+    dark: {
+        car: "车",
+        horse: "马",
+        elephant: "象",
+        bodyguard: "士",
+        general: "将",
+        arms: "卒",
+        cannon: "炮",
+    },
+    red: {
+        car: "车",
+        horse: "马",
+        elephant: "象",
+        bodyguard: "仕",
+        general: "帅",
+        arms: "兵",
+        cannon: "炮",
+    },
+};
+
+const darkChess = chessType.map<ChessPieces>((value, index) => {
+    return {
+        camp: "dark",
+        name: value,
+        top: -radius.value,
+        left: size.value * index - radius.value,
+    };
+});
+
+const redChess = chessType.map<ChessPieces>((value, index) => {
+    return {
+        camp: "red",
+        name: value,
+        top: height.value - radius.value,
+        left: size.value * index - radius.value,
+    };
+});
+
+const chessPieces = reactive<ChessPieces[]>([]);
 </script>
 
 <template>
     <div class="ChineseChess">
-        <div class="checkerboard">
+        <div
+            class="checkerboard"
+            :style="{ width: width + 40 + 'px', height: height + 40 + 'px' }"
+        >
             <template v-for="item in globalParameter.horizontal">
                 <div
                     :style="{
-                        top: `${(item - 1) * 30}px`,
+                        top: `${(item - 1) * size}px`,
                     }"
                     v-if="item !== 6"
                     class="horizontal"
                 ></div>
             </template>
+            <div class="chess">车</div>
+            <div class="chess2"></div>
 
-            <div class="blank" ></div>
+            <div class="blank"></div>
 
             <div
                 :style="{
-                    left: `${(item - 1) * 30}px`,
+                    left: `${(item - 1) * size}px`,
                 }"
                 v-for="item in globalParameter.vertical"
                 class="vertical"
@@ -80,6 +146,33 @@ const chessPieces = reactive({
             top: 121px;
             left: 1px;
             z-index: 6;
+        }
+
+        .chess {
+            position: absolute;
+            top: -13px;
+            left: -13px;
+            z-index: 10;
+            width: 26px;
+            height: 26px;
+            background-color: #f5d7a1;
+            border-radius: 50%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border: 1px solid #180a0a;
+            color: #251d3a;
+        }
+
+        .chess2 {
+            position: absolute;
+            top: -13px;
+            left: 17px;
+            z-index: 10;
+            width: 26px;
+            height: 26px;
+            background-color: palevioletred;
+            border-radius: 50%;
         }
     }
 }
