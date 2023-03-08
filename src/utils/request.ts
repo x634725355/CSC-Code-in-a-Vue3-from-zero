9f5121d027964253422818e34aceea2c20f4e202
澄三彩 <634725355@qq.com>
2023-03-08 12:44:08 +0800

    修改request

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,22 +1,9 @@
-import { boot } from "quasar/wrappers";
-import axios, { AxiosInstance } from "axios";
+import axios from "axios";
 
 export const baseURL =
   process.env.NODE_ENV === 'development' ? 'http://localhost:7777/' : 'https://yiyun.eki.space';
 
 const api = axios.create({ baseURL });
-
-export default boot(({ app }: any) => {
-  // for use inside Vue files (Options API) through this.$axios and this.$api
-
-  app.config.globalProperties.$axios = axios;
-  // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
-  //       so you won't necessarily have to import axios in each vue file
-
-  app.config.globalProperties.$api = api;
-  // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
-  //       so you can easily perform requests against your app's API
-});
 
 export { api };
 
