13b69807bcd89f7e55e56bb59f403e26ca6f1a72
wb_zhaolinjie <wb_zhaolinjie@meituan.com>
2023-05-05 15:49:12 +0800

    copy使用更优化的写法

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,19 +1,19 @@
 // 生成一个20位的hash字符串
 export function getUUID() {
-  return 'xxyyxxyyxxyyxxyyxxyy'.replace(/[xy]/g, function (c) {
-    const r = (Math.random() * 16) | 0;
-    const v = c === 'x' ? r : (r & 0x3) | 0x8;
+    return "xxyyxxyyxxyyxxyyxxyy".replace(/[xy]/g, function (c) {
+        const r = (Math.random() * 16) | 0;
+        const v = c === "x" ? r : (r & 0x3) | 0x8;
 
-    return v.toString(16);
-  });
+        return v.toString(16);
+    });
 }
 
 // 定义一个copy函数，参数为要复制的内容
 export async function copy(text: string) {
-  try {
-    await navigator.clipboard.writeText(text);
-    return true;
-  } catch (error) {
-    return false;
-  }
+    try {
+        await navigator.clipboard.writeText(text);
+        return true;
+    } catch (error) {
+        return false;
+    }
 }
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 export const baseURL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:7777/' : 'https://csc-code-in-a-node-js-from-zero.vercel.app/';
+    process.env.NODE_ENV === "development"
+        ? "http://localhost:7777/"
+        : "https://csc-code-in-a-node-js-from-zero.vercel.app/";
 
 const api = axios.create({ baseURL });
 
 export { api };
-
