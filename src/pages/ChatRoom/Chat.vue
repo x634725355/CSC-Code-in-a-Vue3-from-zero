95ff0e2f3f6103aa0d9eb54119dc1808386f153e
wb_zhaolinjie <wb_zhaolinjie@meituan.com>
2023-09-26 15:52:50 +0800

    更新Chat

---
+++ src/pages/ChatRoom/Chat.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import { onMounted, ref } from "vue";
+import { QInput, QBtn } from "quasar";
+
+const user = ref('')
+const password = ref('')
+
+</script>
+
+<template>
+    <div class="ChatRoom">
+        Chat
+        <div contenteditable="true" >
+
+        </div>
+    </div>
+</template>
+
+<style lang="less" scoped>
+.ChatRoom {
+}
+</style>
--- src/pages/ChatRoom/ChatRoom.vue
+++ src/pages/ChatRoom/ChatRoom.vue
@@ -1,13 +1,24 @@
 <script lang="ts" setup>
 import { onMounted, ref } from "vue";
 import { RouteRecordRaw } from "vue-router";
+import { QInput, QBtn } from "quasar";
 import { routes } from "@/router/routes";
+
+const user = ref('')
+const password = ref('')
 
 </script>
 
 <template>
     <div class="ChatRoom">
         ChatRoom
+        <div>
+            <QInput v-model:model-value="user" ></QInput>
+            <QInput v-model:model-value="password" type="password" ></QInput>
+            <div>
+                <QBtn>登陆</QBtn>
+            </div>
+        </div>
     </div>
 </template>
 
